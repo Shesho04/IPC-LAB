75d7b9d28e175681f52d4c206b88788883fc5a48
Shesho04 <144158305+Shesho04@users.noreply.github.com>
2023-09-05 20:15:34 -0600

    Add files via upload

---
+++ L1-SJSB_1018723.py
@@ -0,0 +1,17 @@
+#Solicitar dos números y calcular la suma, resta, multiplicación y división
+print("Ingrese el primer número: ")
+n1 = float(input())
+print("Ingrese el segundo número: ")
+n2 = float(input())
+
+#Operaciones y resultados
+Suma = n1 + n2
+Resta = n1 - n2
+Multiplicación = n1 * n2
+División = n1 / n2
+
+#Mostrar resultados
+print("la suma es: ", Suma)
+print("la resta es: ", Resta)
+print("la multiplicación es: ", Multiplicación)
+print("la división es: ", División)
